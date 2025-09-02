3e1e5b774a13b1083472aaffd9c55243fa6cac4e
Arun10git <161822531+Arun10git@users.noreply.github.com>
2025-09-02 13:42:44 +0530

    user id is added

--- src/iframes/DemoGithub.java
+++ src/iframes/DemoGithub.java
@@ -2,6 +2,7 @@ package iframes;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,7 +12,8 @@ WebDriver driver;
   @Test
   public void techlearnlogin() {
     driver.get("https://www.techlearn.in/admin/");
-  }
+   driver.findElement(By.xpath("//*[@id=\"user_login\"]")).sendKeys("Arun");
+   }
   @BeforeTest
   public void beforeTest() {
     driver = new ChromeDriver();
