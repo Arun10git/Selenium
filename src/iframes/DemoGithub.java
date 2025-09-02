084226463eb651295ddd5e960bb55bd30ddf03b6
Arun10git <161822531+Arun10git@users.noreply.github.com>
2025-09-02 13:31:43 +0530

    Update DemoGithub.java by purshotham

--- src/iframes/DemoGithub.java
+++ src/iframes/DemoGithub.java
@@ -2,13 +2,21 @@ package iframes;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoGithub {
+WebDriver driver;
+  
   @Test
-  public void f() {
+  public void techlearnlogin() {
+    driver.get("https://www.techlearn.in/admin/");
   }
   @BeforeTest
   public void beforeTest() {
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    
   }
 
 }
